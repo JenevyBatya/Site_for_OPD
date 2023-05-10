782b1012d33df9a862e71ada1330c4b6d35c07f8
Karina <thealisky@gmail.com>
2023-05-10 17:44:14 +0300

    С добавленными описаниями

--- Web/src/main/java/com/example/Web/controllers/ExpertController.java
+++ Web/src/main/java/com/example/Web/controllers/ExpertController.java
@@ -1,17 +1,18 @@
 package com.example.Web.controllers;
 
-import com.example.Web.models.Adjective;
-import com.example.Web.models.Occupation;
-import com.example.Web.models.User;
-import com.example.Web.models.result;
+import com.example.Web.models.*;
 import com.example.Web.repo.AdjectiveRepository;
+import com.example.Web.repo.ExpertsOpinionRepo;
 import com.example.Web.repo.OccupationRepository;
 import com.example.Web.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
 
@@ -23,30 +24,51 @@ public class ExpertController {
     OccupationRepository occupationRepository;
     @Autowired
     AdjectiveRepository adjectiveRepository;
+    @Autowired
+    ExpertsOpinionRepo expertsOpinionRepo;
+
     @GetMapping("/expert")
-    public String startMode(){
+    public String startMode() {
         return "expertsMode";
     }
+
     @GetMapping("/expert/send_test")
-    public String sendTest(Model model){
+    public String sendTest(Model model) {
         Iterable<User> allUsers = userRepo.findAll();
-        model.addAttribute("allUsers",allUsers);
+        model.addAttribute("allUsers", allUsers);
         return "send_test";
     }
+
     @GetMapping("/expert/grading_occupation")
-    public String show(Model model){
+    public String show(Model model) {
         Iterable<Occupation> allOccupations = occupationRepository.findAll();
-        model.addAttribute("allOccupations");
+        model.addAttribute("allOccupations", allOccupations);
         return "occupations";
     }
+
     @GetMapping("/expert/grading_occupation/{id}")
-    public String grade(Model model,@PathVariable(value = "id") int id){
+    public String grade(Model model, @PathVariable(value = "id") int id) {
         Occupation occupation = occupationRepository.findById(id).get();
-        model.addAttribute("allOccupations");
+        model.addAttribute("occupation", occupation);
         Iterable<Adjective> adjectiveList = adjectiveRepository.findAll();
         model.addAttribute("adjectiveList", adjectiveList);
         model.addAttribute("result", new result());
         return "grading_occupation";
     }
 
+    @PostMapping("/expert/grading_occupation/{id}")
+    public String gradeBD(@PathVariable(value = "id") int id, @ModelAttribute("result") result result, Authentication authentication) {
+        int[] selectedIds = result.getSelectedIds();
+        Occupation occupation = occupationRepository.findById(id).get();
+        User user = userRepo.findByEmail(authentication.getName());
+        if (selectedIds != null || selectedIds.length != 0) {
+            for (int adjId : selectedIds) {
+                Adjective adjective = adjectiveRepository.findById(adjId).get();
+                expertsOpinionRepo.save(new ExpertsOpinion(user, occupation, adjective));
+
+            }
+        }
+        return "redirect:/expert/grading_occupation";
+
+    }
 }
---
+++ Web/src/main/java/com/example/Web/repo/ExpertsOpinionRepo.java
@@ -0,0 +1,7 @@
+package com.example.Web.repo;
+
+import com.example.Web.models.ExpertsOpinion;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ExpertsOpinionRepo extends CrudRepository<ExpertsOpinion,Integer> {
+}
